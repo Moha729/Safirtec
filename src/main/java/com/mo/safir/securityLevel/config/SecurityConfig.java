7e42e4f76b7e9008b3c8312578599dae28322c88
moha <moha729g@stud.kea.dk>
2022-11-14 18:22:20 +0100

    fixed post issue

--- src/main/java/com/mo/safir/securityLevel/config/SecurityConfig.java
+++ src/main/java/com/mo/safir/securityLevel/config/SecurityConfig.java
@@ -1,27 +1,31 @@
 package com.mo.safir.securityLevel.config;
 
+import com.mo.safir.securityLevel.service.JpaUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-/*     private final JpaUserDetailsService jpaUserDetailsService;
+   private final JpaUserDetailsService jpaUserDetailsService;
 
     public SecurityConfig(JpaUserDetailsService jpaUserDetailsService) {
         this.jpaUserDetailsService = jpaUserDetailsService;
     }
 
    @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+   SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         return http
-                .csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"))
+                .csrf(/*csrf -> csrf.ignoringAntMatchers("/h2-console/**")*/).disable()
                 .authorizeRequests(auth -> auth
                         .antMatchers("/h2-console/**").permitAll()
                         .mvcMatchers("/api/posts").permitAll()
@@ -30,7 +34,7 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions().sameOrigin())
                 .httpBasic(Customizer.withDefaults())
                 .build();
-    }*/
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
